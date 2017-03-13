4cac6fffae0435b84d4366d2fc190962389e1165
zevaxt <zevaxt@gmail.com>
2017-03-13 15:18:25 -0500

    s

--- src/java/pkgControlador/UsuariosControlador.java
+++ src/java/pkgControlador/UsuariosControlador.java
@@ -11,7 +11,6 @@ import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -21,10 +20,6 @@ import pkgModelo.HelpUsuario;
  *
  * @author EDUARD
  */
-@WebServlet(name = "UsuariosControlador", urlPatterns
-        = {
-            "/UsuariosControlador"
-        })
 
 public class UsuariosControlador extends HttpServlet {
 
